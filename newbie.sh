fbe86ae961adeb293a082163bb3badb497246d18
RANDIOLOY <54799488+RandiSr@users.noreply.github.com>
2019-09-04 09:31:06 +0700

    Add files via upload

---
+++ newbie.sh
@@ -0,0 +1,29 @@
+###SCRIPT RUSAK !!! BELUM PERBAIKAN
+#!/bin/bash
+#version 1.0
+
+pkg install toilet
+
+clear
+printf "  \e[101m\e[1;77m  :::::::::>>>WELCOME<<<:::::::::\e[0m\n"
+printf "  \e[101m\e[1;77m :::MOHON MAAF TOLS SEDANG ADA PERBAIKAN:::\e[0m\n"
+printf "  \e[101m\e[1;77m  :::::::::>>>SABAR YA<<<:::::::::\e[0m\n"
+
+
+# Variables
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+
+
