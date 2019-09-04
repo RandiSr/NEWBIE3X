d58ca2623278088762f0a59af832ff615fed2d76
RANDIOLOY <54799488+RandiSr@users.noreply.github.com>
2019-09-04 09:48:24 +0700

    Add files via upload

---
+++ newbie.sh
@@ -0,0 +1,36 @@
+###SCRIPT RUSAK !!! BELUM PERBAIKAN
+#!/bin/bash
+#version 1.0
+
+pkg install toilet
+
+clear
+
+figlet Randi | lolcat
+
+clear
+printf "  \e[101m\e[1;77m  :::::::::>>>          WELCOME     <<<:::::::::\e[0m\n"
+printf "  \e[101m\e[1;77m :::       MOHON MAAF TOLS SEDANG ADA PERBAIKAN   :::\e[0m\n"
+printf "  \e[101m\e[1;77m  :::::::::>>>TOLS ADA PERBIAKAN MOHON MAAF<<<:::::::::\e[0m\n"
+
+
+# Variables
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+
+
+echo -e $g"       ||||||KONTAK KLO INGIN TOLS NYA|||||"
+sleep 2
+figlet ig  @RanOloy
