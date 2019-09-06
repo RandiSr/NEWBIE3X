61b1218117b4482e06b4402aa0fc1e254d3dc25a
RANDIOLOY <54799488+RandiSr@users.noreply.github.com>
2019-09-06 17:36:16 +0700

    Add files via upload

---
+++ newbie.sh
@@ -0,0 +1,35 @@
+###SCRIPT RUSAK!!
+#!/bin/bash
+
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+mer='\033[41;97m' #Tepi
+st='\033[0m' #Stop
+toilet -f smslant "Script" | lolcat
+sleep 1
+toilet -f smslant "Masih" | lolcat
+sleep 1
+toilet -f smslant "Perbaikan" | lolcat
+sleep 1
+clear
+echo $cy"Author : RandiSr"
+sleep 4
+clear
+echo $pur"Update 10 September 2k19"
+sleep 9
+clear
+sleep 7
+echo $mer"===================="
+echo $bi "Tols Sedang Di tutup"
+echo $mer"===================="
+sleep 2
+clear
+toilet -f smslant "ByE" | lolcat
+sleep 7
+clear
