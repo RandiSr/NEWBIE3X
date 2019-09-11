bb040fdecfca3dc5a23effae2124da7ec988b075
RANDIOLOY <54799488+RandiSr@users.noreply.github.com>
2019-09-11 19:13:32 +0700

    Add files via upload

---
+++ exit.py
@@ -0,0 +1,23 @@
+import random
+import sys
+import time
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.2)
+mengetik('menghubungi Mr.NEWBIE3X')
+mengetik('.')
+mengetik('.')
+mengetik('mencoba meminta akses..')
+mengetik('...')
+mengetik('...')
+mengetik('...')
+mengetik('...')
+mengetik('...')
+mengetik('...')
+mengetik('...')
+mengetik('mengupdate!')
+mengetik('100%')
+mengetik ('adukacapeahdukalierteayakopikopiacan')
+mengetik ('Nuub, Santay GAN!!')
